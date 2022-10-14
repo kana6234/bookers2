c618512211e9c8ebff2d9916de8a18e397f4290a
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-10-14 13:03:30 +0000

    [add]userコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :books, only:[:index, :create, :show, :edit, :update, :destroy]
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
+  resources :users, only:[:index, :show, :edit, :update]
+  resources :books, only:[:index, :create, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
