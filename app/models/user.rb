618b6473a2ed6880a7f63bf4cee0643010b768a3
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-10-20 17:31:47 +0000

    [fix]user、bookモデルのバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,9 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
-  validates :name, length: { minimum: 2 }
+  validates :name, uniqueness: true
+  validates :name, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 75 }
 
   def get_profile_image(width, height)
     unless profile_image.attached?
